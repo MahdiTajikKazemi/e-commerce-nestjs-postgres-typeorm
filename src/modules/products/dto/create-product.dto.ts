ae3619370d5e38b4c469c54de211c7c20258bd8e
Mahdi <mahditajikkazemii@gmail.com>
2025-02-01 01:46:01 +0330

    Add our DTO product + input validators

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsNumber()
+  quantity_in_stock?: number;
+
+  @IsOptional()
+  @IsNumber()
+  unit_price?: number;
+
+  @IsOptional()
+  @IsEnum(['available', 'unavailable'])
+  status?: 'available' | 'unavailable';
+}
