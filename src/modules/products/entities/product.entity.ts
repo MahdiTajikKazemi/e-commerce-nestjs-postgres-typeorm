90d3b9604caefc3d171057cc4b302e50d8bca982
Mahdi <mahditajikkazemii@gmail.com>
2025-02-01 01:44:45 +0330

    Create product entity / model

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,1 +1,38 @@
-export class Product {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'products' })
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  product_id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: 0 })
+  quantity_in_stock: number;
+
+  @Column({ type: 'float', default: 0 })
+  unit_price: number;
+
+  @Column({
+    type: 'enum',
+    enum: ['available', 'unavailable'],
+    default: function () {
+      return this.quantity_in_stock > 0 ? 'available' : 'unavailable';
+    },
+  })
+  status: 'available' | 'unavailable';
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
