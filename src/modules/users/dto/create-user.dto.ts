b06b9f2e77c169504a06719d17861119998bfd99
Mahdi <mahditajikkazemii@gmail.com>
2025-02-01 00:23:29 +0330

    Add validation to user DTO using class validators

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,44 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
-  firstName: string;
-  lastName: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
+  last_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsEnum(['avtice', 'inactive'])
+  @IsOptional()
   status?: 'avtice' | 'inactive';
+
+  @IsEnum(['male', 'female'])
+  @IsOptional()
   gender?: 'male' | 'female';
+
+  @IsEnum(['normal', 'admin', 'gold'])
+  @IsOptional()
   role?: 'normal' | 'admin' | 'gold';
 }
